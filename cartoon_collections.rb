c24c2d5e3ca5017cf1d1a5262decde1e22f9f2b8
Wen Xiao <wennstyle@gmail.com>
2022-04-24 14:49:03 -0400

    commit tree 0d8300b16b81fb5dcdb661db5b5956067dd35a33

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves_array)
+  dwarves_array.each.with_index(1) do |dwarf, num|
+    puts "#{num}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  newArray = array.map {|element| element.capitalize + "!"} #capitalize method uppercase the first letter in each element
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array_ingredients)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array_ingredients.find do |maybe_cheese|
+    cheese_types.include?(maybe_cheese)
+  end
 end
